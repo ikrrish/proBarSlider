32cf6df76e48988b4dbeb382d544fe50410e1a07
Kishan Govindiya <kishangovindiya1@gmail.com>
2023-03-29 12:46:13 +0530

    prBar

--- proBarSlider/ViewController.swift
+++ proBarSlider/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var prBar: UIProgressView!
+   
+    var time = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        prBar.progress = 0.0
+        progress()
     }
 
-
+    func progress(){
+        var a = 0.0
+        //self.prBar.progress = a
+        time = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true, block: { iimer in
+            a+=0.01
+            self.prBar.progress = Float(a)
+            if self.prBar.progress == 1.0{
+                self.time.invalidate()
+                self.view.backgroundColor = .red
+            }
+        
+        })
+    }
 }
 
